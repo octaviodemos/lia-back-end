d806d4e8d29f8eebde9dd07c0f3c2e862c7eb13d
Octávio Demos <octaviodemos@gmail.com>
2025-11-02 20:07:02 -0300

    teste

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import express from 'express';
 import passport from 'passport';
+import cors from 'cors'; 
+
 import { userRoutes } from '@/modules/users/user.routes';
 import { authRoutes } from '@/modules/auth/auth.routes';
 import { bookRoutes } from '@/modules/books/book.routes';
@@ -9,6 +11,24 @@ import { cartRoutes } from '@/modules/cart/cart.routes';
 
 const app = express();
 
+const allowedOrigins = ['http://localhost:4200'];
+
+const corsOptions = {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void,
+  ) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Acesso não permitido pelo CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+
 app.use(express.json());
 app.use(passport.initialize());
 
@@ -16,6 +36,7 @@ app.get('/', (_req, res) => {
   res.status(200).json({ message: 'API do Projeto LIA está no ar!' });
 });
 
+// Rotas da API
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/books', bookRoutes);
